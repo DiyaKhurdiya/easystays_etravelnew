1dd108cc3495bb99a9f39379b8ab3772d444d4c5
Nishtha Das <nishtha3@hotmail.com>
2022-01-25 17:02:08 +0530

    RoomCard + filter issue fixed

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,13 +5,13 @@ const connectDatabase = () => {
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      
     })
     .then((data) => {
       console.log(`MongoDB connected with server: ${data.connection.host}`);
-    }).catch((err)=> {
-        console.log(err)
     })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports = connectDatabase;
